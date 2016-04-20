23a2459e3a9b82ec1e7e5fe86c9cbf85ce916ad2
sys1991 <603442779@qq.com>
2016-04-20 17:12:55 +0800

    Initial commit

---
+++ database01/src/day2.sql
@@ -0,0 +1,60 @@
+SHOW DATABASES;
+USE scott;
+SHOW TABLES;
+# 工资 = 基本工资 + 奖金
+# 1. 查找部门30中员工的详细信息。
+SELECT *
+FROM emp
+WHERE DEPTNO = 30;
+# 2. 找出从事clerk工作的员工的编号、姓名、部门号。
+SELECT
+  EMPNO,
+  ENAME,
+  DEPTNO
+FROM emp
+WHERE JOB = 'clerk';
+# 3. 检索出奖金多于基本工资的员工信息。
+SELECT ENAME
+FROM emp e
+WHERE e.COMM > e.SAL;
+# 4. 检索出奖金多于基本工资60%的员工信息。
+SELECT *
+FROM emp e
+WHERE e.COMM > e.SAL * 0.6;
+# 5. 希望看到10部门的经理或者20部门的职员(clerk)的信息。
+SELECT *
+FROM emp
+WHERE (DEPTNO = 10
+       AND (JOB = 'MANAGER'))
+      OR (DEPTNO = 20
+          AND (JOB = 'CLERk'));
+# 6. 找出10部门的经理、20部门的职员或者既不是经理也不是职员但是高于2000元的员工信息。
+SELECT *
+FROM emp;
+SELECT *
+FROM emp e
+WHERE ((e.DEPTNO = 10
+        AND e.JOB = 'MANAGER')
+       OR (e.DEPTNO =20
+           AND e.JOB = 'CLERk'))
+      OR ((e.DEPTNO = 10 OR e.DEPTNO = 20)AND ((e.JOB!='manager'OR e.JOB!='clerk')
+          AND (e.SAL + e.COMM > 2000)));
+# 7. 找出获得奖金的员工的工作。
+# 8. 找出奖金少于100或者没有获得奖金的员工的信息。
+# 9. 查找员工雇佣日期中当月的最后一天雇佣的。
+# 10. 检索出雇佣年限超过12年的员工信息。
+# 11. 找出姓名以A、B、S开始的员工信息。
+# 12. 找到名字长度为7个字符的员工信息。
+# 13. 名字中不包含R字符的员工信息。
+# 14. 找出员工名字的前3个字符。
+# 15. 将名字中A改为a。
+# 16. 将员工的雇佣日期拖后10年。
+# 17. 返回员工的详细信息并按姓名排序。
+# 18. 返回员工的信息并按员工的工作年限降序排列。
+# 19. 返回员工的信息并按工作降序工资升序排列。
+# 20. 返回员工的姓名、雇佣年份和月份并且按月份和雇佣日期排序。
+# 21. 计算员工的日薪(按30天)。
+# 22. 找出2月份雇佣的员工。
+# 23. 至今为止，员工被雇佣的天数。
+# 24. 找出姓名中包含A的员工信息。
+# 25. 计算出员工被雇佣了多少年、多少月、多少日。
